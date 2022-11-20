08e1c784dd121b9798d0161d6ae5b45cf3b2947e
Oshri Fatkiev <105358126+OshriFatkiev@users.noreply.github.com>
2022-11-20 12:15:50 +0200

    Add files via upload

--- binary_perceptron.m
+++ binary_perceptron.m
@@ -5,9 +5,9 @@ clear
 global N_REPS;
 N_REPS = 50;
 global T_MAX;
-T_MAX = 10^5;
+T_MAX = 10^4;
 
-% Plotting the number of epochs till convergence for P=10, N=[10,20,100]
+% Plotting the number of epochs till convergence for P=10, N=10,20,100
 P = 10;
 for N = [10, 20, 100]
     epochs_arr = zeros(1, N_REPS);
@@ -26,19 +26,17 @@ xlabel('n')
 ylabel('epochs')
 grid on
 hold off
+drawnow
 
 % Plotting the probability of convergence as a function of alpha=P/N
-figure
-for N = [20] %[5, 20, 100]
+for N = [5, 20, 100]
+    figure
     P = 1:N/5:3*N;
     alpha = P ./ N;
     prob_converged = zeros(1, length(P));
-    e1 = zeros(1, length(P));
     e2 = zeros(1, length(P));
     e3 = zeros(1, length(P));
-    e4 = zeros(1, length(P));
     for i = 1:length(P)
-        disp(P(i))
         count = 0; 
         converged_arr = zeros(1, N_REPS);
         epochs_arr = zeros(1, N_REPS);
@@ -47,22 +45,21 @@ for N = [20] %[5, 20, 100]
             [~, converged_arr(n), epochs_arr(n)] = perceptron(X, y0);
         end
         prob_converged(i) = sum(converged_arr == 1) / N_REPS;
-        e1(i) = sum(epochs_arr(converged_arr == 1) <= 10) / N_REPS;
-        e2(i) = sum(epochs_arr(converged_arr == 1) <= 100) / N_REPS;
-        e3(i) = sum(epochs_arr(converged_arr == 1) <= 1000) / N_REPS;
-        e4(i) = sum(epochs_arr(converged_arr == 1) <= 10000) / N_REPS;
+        indexes_list = find(converged_arr == 1);
+        e2(i) = sum(epochs_arr(indexes_list) <= 10^2) / N_REPS;
+        e3(i) = sum(epochs_arr(indexes_list) <= 10^3) / N_REPS;
     end
     plot(alpha, prob_converged, '--o')
     hold on
-    plot(alpha, e1, '--o')
-    plot(alpha, e2, '--o')
     plot(alpha, e3, '--o')
-    plot(alpha, e4, '--o')
+    plot(alpha, e2, '--o')
+    legend('t_{max}=10^4','t_{max}=10^3','t_{max}=10^2')
+    xlabel('$\alpha$', 'interpreter', 'latex')
+    ylabel('$Pr$[converged]', 'interpreter', 'latex')
+    grid on
+    hold off
+    drawnow
 end
-xlabel('alpha')
-ylabel('probability')
-grid on
-hold off
 
 function [X, y0] = generate_data(N, P)
 % Generating random dichotomies to the perceptron
@@ -72,8 +69,8 @@ end
 
 function [w, converged, epochs] = perceptron(X, y0)
 % Implementing a simple binary-perceptron w/ a sign activation function
-    [N, P] = size(X);
-    w = ones(N, 1); 
+    [N, ~] = size(X);
+    w = rand(N, 1); 
     global T_MAX;
     epochs = 0;
     eta = 10^-2;
@@ -82,7 +79,7 @@ function [w, converged, epochs] = perceptron(X, y0)
         converged = true;
         res = (w' * X) .* y0; 
         violation_indexes = find(res < 0);
-        for i = 1:violation_indexes    
+        for i = violation_indexes    
             w = w + eta .* X(:, i) * y0(i);
             % plot(w, '.:'); title(epochs); drawnow 
             converged = false;
